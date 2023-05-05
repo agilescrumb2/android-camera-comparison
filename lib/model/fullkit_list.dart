5fd68f08610af0c87708ccf4781b0a084950330e
WhiteSpec <fadhsyarif@gmail.com>
2023-05-05 23:48:54 +0700

    model

---
+++ lib/model/fullkit_list.dart
@@ -0,0 +1,42 @@
+class Fullkit{
+  String nama_kamera;
+  String mode_af;
+  String fokus_manual;
+  String builtin_flash;
+  String mode_flash;
+  String panjang_fokus;
+  String bidik_perdetik;
+  String zoom_digi;
+  String iso;
+  String resolusi_gambar;
+  String stabilizer;
+  String ukuran_lcd;
+  String resolusi_lcd;
+  String sensor;
+  String white_balance;
+  String harga;
+  String image;
+  bool wish;
+
+  Fullkit({
+    required this.nama_kamera,
+    required this.mode_af,
+    required this.fokus_manual,
+    required this.builtin_flash,
+    required this.mode_flash,
+    required this.panjang_fokus,
+    required this.bidik_perdetik,
+    required this.zoom_digi,
+    required this.iso,
+    required this.resolusi_gambar,
+    required this.stabilizer,
+    required this.ukuran_lcd,
+    required this.resolusi_lcd,
+    required this.sensor,
+    required this.white_balance,
+    required this.harga,
+    required this.image,
+    required this.wish
+
+  });
+}
---
+++ lib/model/lensa_list.dart
@@ -0,0 +1,28 @@
+class Lensa{
+  String name;
+  String bobot;
+  String diameter;
+  String apeture;
+  String filter;
+  String fokus;
+  String zoom;
+  String diafragma;
+  String harga;
+  String image;
+  bool wish;
+
+  Lensa({
+    required this.name,
+    required this.bobot,
+    required this.diameter,
+    required this.apeture,
+    required this.filter,
+    required this.fokus,
+    required this.zoom,
+    required this.diafragma,
+    required this.harga,
+    required this.image,
+    required this.wish,
+
+  });
+}
